427b1ee2b3aff61f93282d2899272b646a9b50a1
Dmitry Mizernik <dimamizernik@gmail.com>
2021-01-25 23:53:42 +0300

    publish batch file

---
+++ backend/publish.bat
@@ -0,0 +1,3 @@
+cd WebApplication
+start "" "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" /p:configuration=debug /p:PublishProfile=FolderProfile /p:DeployOnBuild=true
+pause
