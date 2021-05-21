d5e157e873218fe7f066a70bfa218593de8bb215
ilara90 <68242308+ilara90@users.noreply.github.com>
2021-05-21 20:16:56 +0300

    Publish changes

--- backend/publish.bat
+++ backend/publish.bat
@@ -1,3 +1,4 @@
 cd WebApplication
-C:\\"Program Files (x86)"\\"Microsoft Visual Studio"\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe /p:configuration=debug /p:PublishProfile=FolderProfile /p:DeployOnBuild=true
+C:\\"Program Files (x86)"\\"Microsoft Visual Studio"\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe -t:restore
+C:\\"Program Files (x86)"\\"Microsoft Visual Studio"\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe /p:configuration=debug /p:PublishProfile=FolderProfile /p:DeployOnBuild=true 
 pause
